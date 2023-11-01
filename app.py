8955794de7ccf25b832820cdffae5ee0efe4186b
Johanna Hakonen <48389769+hannahakonen@users.noreply.github.com>
2023-11-01 11:08:07 +0200

    grinberg ch6 email added

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate   
+from flask_mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -15,11 +17,20 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io' #smtp.googlemail.com
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -40,6 +51,19 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+    
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 class NameForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -68,13 +92,15 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            db.session.commit()
+            db.session.commit() #missing from the book but is in the code
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
-        form.name.data = ''
+        #form.name.data = '' #is in the book but not in the code
         return redirect(url_for('index'))
-    return render_template('index.html',
-        form=form, name=session.get('name'),
-        known=session.get('known', False))
+    return render_template('index.html', form=form, name=session.get('name'), 
+                           known=session.get('known', False))
